1305e5834b8586c5eb41a5f1e11601e9c8eaccf5
etheng-jiao <75689659+etheng-jiao@users.noreply.github.com>
2021-05-01 05:16:02 +0200

    add treatment plot

--- R/05_visualization.R
+++ R/05_visualization.R
@@ -119,3 +119,85 @@ ggplot(data = longer_data,
   theme(legend.position = "bottom") +
   facet_wrap(vars(outcome), ncol = 2)
 
+# Treatment plot
+
+my_data <- read_tsv(file = "data/03_my_data_clean_aug.tsv")%>% 
+           mutate(Treatment = factor(Treatment))%>%
+           mutate(bm=factor(bm))%>%
+           mutate(outcome= case_when(outcome == "0"~"alive",
+                                     outcome=="1"~"death"))
+p1<-ggplot(data = my_data,
+       mapping = aes(x = outcome,
+                      y= age, fill=Treatment)) +
+  geom_boxplot(alpha = 0.5, show.legend = FALSE) +
+  theme_classic()
+  
+p2<-ggplot(data = my_data,
+       mapping = aes(x = outcome,
+                      y= wt,fill=Treatment)) +
+  geom_boxplot(alpha = 0.5) +
+  labs(caption = "Data from D.P.Byar(1977)")+
+  theme_classic()+
+  theme(legend.position = "bottom")
+
+p3<-ggplot(data = my_data,
+       mapping = aes(x = outcome,
+                      y= ap,fill=Treatment)) +
+  geom_boxplot(alpha = 0.5) +
+  labs(caption = "Data from D.P.Byar(1977)")+
+  theme_classic()+
+  theme(legend.position = "bottom")
+
+p4<-ggplot(data = my_data,
+       mapping = aes(x = outcome,
+                      y= sz,fill=Treatment)) +
+  geom_boxplot(alpha = 0.5, show.legend = FALSE) +
+  theme_classic()
+
+p5<-ggplot(data = my_data,
+       mapping = aes(x = outcome,
+                      y= rx,fill=Treatment)) +
+  geom_boxplot(alpha = 0.5, show.legend = FALSE) +
+  theme_classic()
+
+p6<-ggplot(data = my_data,
+       mapping = aes(x = outcome,
+                      y= dbp,fill=Treatment)) +
+  geom_boxplot(alpha = 0.5,show.legend = FALSE) +
+  theme_classic()
+
+p7<-ggplot(data = my_data,
+       mapping = aes(x = outcome,
+                      y= sbp,fill=Treatment)) +
+  geom_boxplot(alpha = 0.5) +
+  labs(caption = "Data from D.P.Byar(1977)")+
+  theme_classic()+
+  theme(legend.position = "bottom")
+
+p8<-ggplot(data = my_data,
+       mapping = aes(x = outcome,
+                      y= hg,fill=Treatment)) +
+  geom_boxplot(alpha = 0.5) +
+  labs(caption = "Data from D.P.Byar(1977)")+
+  theme_classic()+
+  theme(legend.position = "bottom")
+
+p9<-ggplot(data = my_data,
+       mapping = aes(x = outcome,
+                      y= sg,fill=Treatment)) +
+  geom_boxplot(alpha = 0.5) +
+  labs(caption = "Data from D.P.Byar(1977)")+
+  theme_classic()+
+  theme(legend.position = "bottom")
+
+p1+p2
+p4+p9
+p6+p7
+p5+p8
+p3
+
+
+
+
+
+
