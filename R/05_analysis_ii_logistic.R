4ee5146eba9ec5c3169fc4507bd681fc36fcdf8a
signemarcussen <engis-10@hotmail.com>
2021-05-01 20:01:37 +0000

    working on logistic regression

--- R/05_analysis_ii_logistic.R
+++ R/05_analysis_ii_logistic.R
@@ -11,23 +11,48 @@ source(file = "R/99_project_functions.R")
 
 
 # Load data ---------------------------------------------------------------
-prostate_clean_aug <- read_tsv(file = "data/03_my_data_clean_aug.tsv")
+prostate_clean_aug <- read_rds(file = "data/03_prostate_clean_aug.rds.gz")
 
 
 # Wrangle data ------------------------------------------------------------
-prostate_clean_aug %>% ...
+
+# 
+prostate_long_nested <- prostate_clean_aug %>%
+  select(-status, -treatment, -age_group) %>%
+  pivot_longer(cols = c(-patno, -outcome), 
+               names_to = "variable", 
+               values_to = "value") %>%
+  group_by(variable) %>%
+  nest() %>%
+  ungroup()
+
+prostate_clean_aug %>%
+  select(-status, -treatment, -age_group) %>%
+  group_by() %>% 
+  nest()
 
 
 # Model data --------------------------------------------------------------
-#Creating a logistic model
-## We will try to use the age of the patients to predict the risk for the outcome 
-model_age <- my_data_clean_aug %>% 
-  glm(outcome ~ age,
+## Creating a logistic model
+
+prostate_logistic <- prostate_clean_aug %>% 
+  glm(outcome ~ treatment_mg,
       data = .,
       family = binomial(link = "logit"))
 
+prostate_logistic <- prostate_clean_aug %>% 
+  drop_na %>% 
+  glm(outcome ~ stage + CVD + hg + tumor_size + age,
+      data = .,
+      family = binomial(link = "logit"))
+tidy(prostate_logistic)
+
+
+
+## Maybe try another variable to predict outcome - fx treatment?
+
+
 # What can we say about this? 
-summary(model_age)
 
 
 # Visualise data ----------------------------------------------------------
