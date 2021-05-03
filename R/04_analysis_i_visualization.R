065bfe839f67e56c8591f9eb1a45323976652ec7
signemarcussen <engis-10@hotmail.com>
2021-05-03 09:02:37 +0000

    working on visualizations

--- R/04_analysis_i_visualization.R
+++ R/04_analysis_i_visualization.R
@@ -20,23 +20,36 @@ prostate_clean_aug <- read_rds(file = "data/03_prostate_clean_aug.rds.gz")
 
 # Wrangle data ------------------------------------------------------------
 
-
+prostate_clean_aug <- prostate_clean_aug %>% 
+  mutate(outcome = case_when(outcome == 0 ~ "alive",
+                             outcome == 1 ~ "dead"))
+         #outcome = factor(outcome)))
 
 # Visualise data ----------------------------------------------------------
+
+## Distribution of alive/dead
+prostate_clean_aug %>% 
+  ggplot(mapping = aes(outcome,
+                       fill = outcome)) +
+  geom_bar(alpha = 0.8, show.legend = FALSE) +
+  theme_minimal()
 
 ## Distribution of alive/dead for each treatment, scaled for better comparison
 ## across groups
 prostate_clean_aug %>% 
   ggplot(mapping = aes(treatment,
                        fill = outcome)) +
-  geom_bar(position = "fill")
+  geom_bar(alpha = 0.8, position = "fill") +
+  facet_wrap(~stage)
 
 ## Boxplots of tumor_size for each status after treatment with 1mg
 prostate_clean_aug %>% 
-  filter(treatment_mg == "1.0") %>% 
+  #filter(treatment_mg == 0 | treatment_mg == "1.0") %>% 
   ggplot(mapping = aes(tumor_size,
                        fill = outcome)) +
-  geom_boxplot()
+  geom_boxplot() +
+  facet_wrap(~treatment_mg) +
+  coord_flip()
 
 ## Scatterplot of age vs tumor size
 prostate_clean_aug %>% 
@@ -45,7 +58,7 @@ prostate_clean_aug %>%
   geom_point()
 
 ## Barplot/histogram of status colored by treatment
-p1 <- prostate_clean_aug %>%
+prostate_clean_aug %>%
   distinct(patno,stage,treatment,status) %>%
   count(status,treatment) %>%
   ggplot(aes(y = status,x = n))+
@@ -55,51 +68,19 @@ p1 <- prostate_clean_aug %>%
                 base_size = 12)+
   theme(legend.position = "none")
 
-## This plot doesn't make sense?
-p2 <- prostate_clean_aug %>%
-  distinct(patno,stage,hg,status) %>%
-  ggplot(aes(y = status, x = hg))+
-  geom_point(aes(colour = stage))+
-  theme_classic(base_family = "Avenir",
-                base_size = 12)+
-  theme(legend.position = "bottom")
-
-## This plot doesn't make sense?
-prostate_clean_aug %>% 
-  drop_na %>% 
-  ggplot(mapping = aes(x = treatment_mg,
-                       y = outcome,
-                       fill = age_group)) +
-  geom_boxplot()
-
-## This doesn't give anything?
-# Investigating correlation 
-prostate_clean_aug %>% select(.data = ., treatment_mg, age, hg) %>% 
-  cor(use = "pairwise.complete.obs", method = "pearson")
-
-## Not supposed to use this package..
-ggpairs(prostate_clean_aug,
-        mapping = aes(color = outcome),
-        columns = c("Treatment", "hg", "age", "stage", "outcome"))
-
-## Not really useful ... 
-ggplot(data = prostate_clean_aug,
-       mapping = aes(x = age,
-                     y = treatment_mg,
-                     color = outcome)) +
-  geom_point()
 
 ## Boxplot of age for each status colored by Bone Metastasis
-p1<-ggplot(data = prostate_clean_aug,
+ggplot(data = prostate_clean_aug,
            mapping = aes(x = outcome,
-                         y= age, fill = bone_mets)) +
-  geom_boxplot(alpha = 0.5, show.legend = FALSE) +
-  scale_fill_manual(labels = c("Non_Metastases", "Metastases"),
-                    values = c("red","blue"))+
-  theme_classic()
+                         y = tumor_size, 
+                         fill = treatment)) +
+  geom_boxplot(alpha = 0.5, show.legend = TRUE) +
+  #scale_fill_manual(labels = c("Non_Metastases", "Metastases"),
+                    #values = c("red","blue"))+
+  theme_minimal()
 
 ## Boxplot of Weight Index for each status colored by Bone Metastasis
-p2<-ggplot(data = prostate_clean_aug,
+ggplot(data = prostate_clean_aug,
            mapping = aes(x = outcome,
                          y=  weight_index, fill= bone_mets)) +
   geom_boxplot(alpha = 0.5) +
@@ -167,23 +148,9 @@ p4+p6
 p7+p8
 p3
 
-## Not supposed to use this package..
-#just a try
-library("ggridges")
-ggplot(data = longer_data,
-       mapping = aes(x = value,
-                     y = parameter,
-                     fill = parameter)) +
-  geom_density_ridges(alpha = 0.5) +
-  scale_fill_viridis_d() +
-  labs(title = "Distribution for different parameter") +
-  theme_minimal(base_family = "Avenir", base_size = 12) +
-  theme(legend.position = "bottom") +
-  facet_wrap(vars(outcome), ncol = 2)
-
 
 ## Boxplots colored according to treatment
-p1<-ggplot(data = prostate_clean_aug,
+p1 <- ggplot(data = prostate_clean_aug,
            mapping = aes(x = outcome,
                          y= age, fill=treatment_mg)) +
   geom_boxplot(alpha = 0.5, show.legend = FALSE) +
