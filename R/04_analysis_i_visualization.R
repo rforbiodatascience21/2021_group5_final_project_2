5178cd73ccf7b222e79d4fc97eae4cdca4878e7f
LucilleAugusta <s184250@student.dtu.dk>
2021-05-06 12:59:53 +0000

    beautifying plots

--- R/04_analysis_i_visualization.R
+++ R/04_analysis_i_visualization.R
@@ -25,6 +25,7 @@ prostate_clean_aug <- prostate_clean_aug %>%
          treatment = factor(treatment),
          stage = factor(stage),
          performance_lvl = factor(performance_lvl),
+         EKG_lvl = factor(EKG_lvl),
          outcome = case_when(outcome == 0 ~ "alive",
                              outcome == 1 ~ "dead"))
          
@@ -35,80 +36,69 @@ prostate_clean_aug <- prostate_clean_aug %>%
 ########################################
 ### Plots of pre-treatment variables ###
 ########################################
-# I think if we want to investigate the data before treatment, we should select the data when treament_mg = 0 
 
 # Distribution plot for the numeric variables stratified on "stage" 
 prostate_clean_aug_longer <- prostate_clean_aug %>% 
-  select(patient_ID, stage, where(is.numeric), -treatment_mg) %>% 
+  select(patient_ID, stage, where(is.numeric), -treatment_mg, -acid_phosphatase) %>% 
   pivot_longer(cols = c(-patient_ID, -stage), names_to = "variables", values_to = "values")
+
+variables_names <- c("acid_phosphatase_log" = "log( Acid Phosphatase )",
+                     "age" = "Age [years]",
+                     "dbp" = "dbp", 
+                     "EKG_lvl" = "EKG level",
+                     "EKG" = "Electrocardiography",
+                     "hemoglobin" ="Hemoglobin [g/100ml]",
+                     "sbp" ="sbp",
+                     "tumor_size" = "Tumor Size [cm^2]",
+                     "weight_index" = "Weight Index",
+                     "performance" = "Performace",
+                     "performance_lvl" = "Performance level", 
+                     "bone_mets" = "Bone Metastases",
+                     "CVD" = "History of Cardiovascular Disease")
 
 ggplot(prostate_clean_aug_longer, 
        mapping = aes(values, 
                      fill = stage)) +
   geom_density(alpha = 0.6) +
-  facet_wrap(~ variables, scales = "free", ncol = 3) +
+  facet_wrap(~ variables, 
+             scales = "free", 
+             labeller = labeller(variables = variables_names)) +
   labs(x = " ", 
        y = "Density",
        title = "Distribution of the Pre-treatment Variables",
        subtitle = "The distribution of the numeric variables for the pre-treatment measures",
-       fill = "Stage") +
+       fill = "Stage of prostate cancer") +
   theme_minimal() + 
-  theme(legend.position = "bottom") +
+  theme(legend.position = c(0.85, 
+                            0.1)) +
   scale_color_economist() +
   scale_fill_economist()
-# We can use fct_inorder() to set the order of the plots
-
 
 # Distribution of the categorical variables stratified on "stage" 
-# CVD, bone_mets, performance_lvl
+# CVD, bone_mets, performance, EKG
 
 prostate_clean_aug %>%
-  select(patient_ID, stage, CVD, bone_mets, performance_lvl) %>%
+  select(patient_ID, stage, CVD, bone_mets, performance, EKG) %>%
+  drop_na() %>% 
   pivot_longer(cols = c(-patient_ID, -stage), names_to = "variables", values_to = "values") %>% 
   ggplot(mapping = aes(values,
                        fill = stage)) +
   geom_bar() +
-  facet_wrap(~ variables, scales = "free") +
+  facet_wrap(~ variables,
+             scale = "free_x",
+             nrow = 1,
+             labeller = labeller(variables = variables_names)) +
+  theme_minimal() + 
+  theme(axis.text.x = element_text(angle = 35, 
+                                   hjust = 1),
+        plot.title = element_text(face = "bold", size = 16),
+        plot.subtitle = element_text(face = "italic")) +
   labs(x = " ", 
        y = "Number of cases",
-       title = "Distribution of the Pre-treatment Variables",
-       subtitle = "The distribution of the categorical variables for the pre-treatment measures",
+       title = "Distribution of the Pre-treatment Categorical Variables",
+       subtitle = "Bar plots of the categorical variables for the pre-treatment measurements, stratified on the stage of Prostate cancer",
        fill = "Stage") +
-  theme_minimal() + 
   scale_fill_economist()
-  
-# Same as above - slightly different layout 
-CVD_plot <- prostate_clean_aug %>%
-  ggplot(mapping = aes(CVD,
-                       fill = stage)) +
-  geom_bar(show.legend = FALSE) +
-  theme_minimal() + 
-  scale_color_economist() + 
-  scale_fill_economist()
-
-BM_plot <- prostate_clean_aug %>%
-  ggplot(mapping = aes(bone_mets,
-                       fill = stage)) +
-  geom_bar(show.legend = FALSE) +
-  theme_minimal() + 
-  scale_color_economist() + 
-  scale_fill_economist()
-
-performance_plot <- prostate_clean_aug %>%
-  ggplot(mapping = aes(performance_lvl,
-                       fill = stage)) +
-  geom_bar(show.legend = TRUE) +
-  theme_minimal() + 
-  scale_color_economist() + 
-  scale_fill_economist()
-
-
-CVD_plot + BM_plot + performance_plot +
-  plot_annotation( title = "Distribution of the Pre-treatment Variables",
-                   subtitle = "The distribution of the categorical variables for the pre-treatment measures") +
-  plot_layout(guides = "collect") 
-
-# Must fix axis-label 
 
 # Heatmap of correlations between the numeric variables
 prostate_clean_aug %>% 
