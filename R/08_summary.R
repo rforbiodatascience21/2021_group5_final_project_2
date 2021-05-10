f26e9836388108347889e7f52c0b8a71278851bc
LucilleAugusta <s184250@student.dtu.dk>
2021-05-10 17:02:54 +0000

    using tidy for summary table

--- R/08_summary.R
+++ R/08_summary.R
@@ -8,72 +8,18 @@ library("tidyverse")
 source(file = "R/99_project_functions.R")
 
 # Load data and wrangle----------------------------------------------------
-my_data <- read_tsv(file = "data/03_prostate_clean_aug.tsv.gz")
+prostate_clean_aug <- read_tsv(file = "data/03_prostate_clean_aug.tsv.gz")
 
-num_data<- my_data %>% select(patient_ID, stage, outcome, where(is.numeric))%>%drop_na 
-                        
-longer_data <- num_data %>% pivot_longer(cols = c(-patient_ID,-outcome,-stage), 
-                            names_to = "variables", 
-                            values_to = "values")
-# Summary (total)----------------------------------------------------------
-total_table <- longer_data %>% group_by(variables) %>% 
-                summarise(n = n(),
-                          max_value = max(values),
-                          min_value = min(values),
-                          mean_value = mean(values),
-                          sigma_value = sd(values))
+# Factorizing variables 
+prostate_clean_aug <- prostate_clean_aug %>%
+  mutate(bone_mets = factor(bone_mets),
+         CVD = factor(CVD),
+         treatment = factor(treatment),
+         stage = factor(stage),
+         performance_lvl = factor(performance_lvl),
+         EKG_lvl = factor(EKG_lvl))
 
-# Summary by outcome ------------------------------------------------------
-
-alive_long_data <- longer_data %>% filter(outcome == 0)
-death_long_data <- longer_data %>% filter(outcome == 1)
-
-
-table_alive <- alive_long_data %>% group_by(variables) %>% 
-               summarise(n = n(),
-                         max_value = max(values),
-                         min_value = min(values),            
-                         mean_alive = mean(values),
-                         sigma_alive = sd(values))
-
-table_death <- death_long_data %>% group_by(variables) %>% 
-               summarise(n = n(),
-                         max_value = max(values),
-                         min_value = min(values),            
-                         mean_alive = mean(values),
-                         sigma_alive = sd(values))
-
-
-SummaryByOutcome_table<- full_join(table_alive,table_death,by="variables")
-
-# Summary by stage ------------------------------------------------------
-
-stage3_long_data <- longer_data %>% filter(stage == 3)
-stage4_long_data <- longer_data %>% filter(stage == 4)
-
-
-table_stage3 <- stage3_long_data %>% group_by(variables) %>% 
-               summarise(n = n(),
-                         max_value = max(values),
-                         min_value = min(values),            
-                         mean_alive = mean(values),
-                         sigma_alive = sd(values))
-
-table_stage4 <- stage4_long_data %>% group_by(variables) %>% 
-               summarise(n = n(),
-                         max_value = max(values),
-                         min_value = min(values),            
-                         mean_alive = mean(values),
-                         sigma_alive = sd(values))
-
-
-SummaryByStage_table<- full_join(table_stage3,table_stage4,by="variables")
-
-
-# Write data --------------------------------------------------------------
-write_tsv(x = SummaryByOutcome_table,
-          file = "data/SummaryByOutcome_table.tsv")
-write_tsv(x = SummaryByStage_table,
-          file = "data/SummaryByStage_table.tsv")
-write_tsv(x = total_table,
-          file = "data/total_table.tsv")
+# Summary statistic 
+prostate_clean_aug %>% 
+  select(where(is.numeric), -patient_ID) %>% 
+  tidy()
